2793516e8c7ce2235b968b13c4efea23ce12503c
Harun29 <ibrahimovic.harun01@gmail.com>
2025-04-08 20:06:07 +0200

    Finished bitrate calculation exercise. Created Models, Services and Parsers and demonstrated it in the Main.

---
+++ BitrateCalculator/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrateCalculator.Models
+{
+    public class Device
+    {
+        public string DeviceName { get; set; }
+        public string Model { get; set; }
+        public List<NetworkInterface> NICs { get; set; }
+    }
+}
---
+++ BitrateCalculator/Services/BpsCalculator.cs
@@ -0,0 +1,27 @@
+﻿using BitrateCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitrateCalculator.Services
+{
+    public class BpsCalculator : IBpsCalculator
+    {
+        public BitrateResult Calculate(NetworkInterface current, NetworkInterface previous, double intervalSeconds)
+        {
+            return new BitrateResult
+            {
+                RxBitrate = CalculateBitrate(current.Rx, previous.Rx, intervalSeconds),
+                TxBitrate = CalculateBitrate(current.Tx, previous.Tx, intervalSeconds)
+            };
+        }
+
+        private double CalculateBitrate(long currentValue, long previousValue, double intervalSeconds)
+        {
+            long difference = currentValue - previousValue;
+            return (difference * 8.0) / intervalSeconds;
+        }
+    }
+}
